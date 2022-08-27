42c0e4bd0c79d94f4723238b76d0d20f4eff57d9
Shun <ter0123540@gmail.com>
2022-08-27 15:30:14 +0800

    Calc days in each month

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const input = document.querySelector(".date-picker");
 const targetYear = document.querySelector(".target-year");
 const targetMonth = document.querySelector(".target-month");
+const targetDates = document.querySelector(".target-dates");
 const yearMonthArea = document.querySelector(".year-month-area");
 
 const months = [
@@ -26,11 +27,17 @@ yearMonthArea.addEventListener("click", changeMonth);
 
 function changeMonth(e) {
   const behavior = e.target.getAttribute("class");
+  if (!behavior.includes("fa-solid")) return;
   if (behavior.includes("fa-angle-left")) {
     month -= 1;
   } else if (behavior.includes("fa-angle-right")) {
     month += 1;
   }
+  checkMonthLimit();
+  render();
+}
+
+function checkMonthLimit() {
   if (month > 11) {
     month = 0;
     year += 1;
@@ -38,17 +45,26 @@ function changeMonth(e) {
     month = 11;
     year -= 1;
   }
-  render();
 }
 
-function init() {
-  targetYear.textContent = year;
-  targetMonth.textContent = months[month];
+function calcDates() {
+  if (month === 3 || month === 5 || month == 8 || month === 10) {
+    return 30;
+  } else if (month === 1) {
+    return year % 4 === 0 ? 29 : 28;
+  } else {
+    return 31;
+  }
 }
 
 function render() {
   targetYear.textContent = year;
   targetMonth.textContent = months[month];
+  let dates = ``;
+  for (let i = 0; i < calcDates(); i++) {
+    dates += `<span class="date">${i + 1}</span>`;
+  }
+  targetDates.innerHTML = dates;
 }
 
-init();
+render();
